4ac92b8f88d86020031b159b12d50a7a62d2ef81
kuzdogan <kaanuzdogan@hotmail.com>
2020-05-08 16:15:13 +0200

    ESLint for client

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar(props) {
+
+  return (
+    <header>
+      <div className='navbar'>
+        Blogletter
+      </div>
+    </header>
+  );
+}
